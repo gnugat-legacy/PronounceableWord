99dd523d00f4c1da431defdec15583052d3dc094
Loïc Chardonnet <loic.chardonnet@gmail.com>
2011-12-05 17:03:14 +0000

    [EnunciableWordGenerator] Test: removing the random length, using an incrementing length instead.

--- test/EnunciableWordGeneratorTest.php
+++ test/EnunciableWordGeneratorTest.php
@@ -3,19 +3,17 @@ require '../EnunciableWordGenerator.php';
 
 class EnunciableWordGeneratorTest extends PHPUnit_Framework_TestCase {
     public function testLength() {
-        $minimumLength = 1;
-        $maximumLength = getrandmax();
-
+        $length = 1;
         $maximumTestNumber = 1000;
         for ($currentTestNumber = 0; $currentTestNumber < $maximumTestNumber; $currentTestNumber++) {
             $enunciableWordGenerator = new EnunciableWordGenerator();
 
-            $length = rand($minimumLength, $maximumLength);
             $enunciableWordGenerator->length = $length;
 
             $generatedWord = $enunciableWordGenerator->generate();
 
             $this->assertEquals($length, strlen($generatedWord));
+            $length++;
         }
     }
 }
