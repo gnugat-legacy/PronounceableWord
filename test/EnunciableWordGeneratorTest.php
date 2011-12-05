036cc1dab677e0f566119c6704c3fc1ac25c635d
Loïc Chardonnet <loic.chardonnet@gmail.com>
2011-12-05 11:19:24 +0000

    [EnunciableWordGenerator] Test: Changing length management, and setting a minimum and maximum length for the rand function.

--- test/EnunciableWordGeneratorTest.php
+++ test/EnunciableWordGeneratorTest.php
@@ -4,13 +4,14 @@ require '../EnunciableWordGenerator.php';
 class EnunciableWordGeneratorTest extends PHPUnit_Framework_TestCase {
     public function testLength() {
         $minimumLength = 1;
+        $maximumLength = getrandmax();
 
         $maximumTestNumber = 1000;
         for ($currentTestNumber = 0; $currentTestNumber < $maximumTestNumber; $currentTestNumber++) {
             $enunciableWordGenerator = new EnunciableWordGenerator();
 
-            $length = rand($minimumLength);
-            $enunciableWordGenerator->setLength($length);
+            $length = rand($minimumLength, $maximumLength);
+            $enunciableWordGenerator->length = $length;
 
             $generatedWord = $enunciableWordGenerator->generate();
 
