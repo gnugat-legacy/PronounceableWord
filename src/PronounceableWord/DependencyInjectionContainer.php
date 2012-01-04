b664ce1678f0edd341d9220dc9cbbcdf4f0df5a5
Loïc Chardonnet <loic.chardonnet@gmail.com>
2012-01-04 09:52:00 +0000

    [DependencyInjectionContainer] Creating the test with its class

---
+++ src/PronounceableWord/DependencyInjectionContainer.php
@@ -0,0 +1,12 @@
+<?php
+/*
+ * This file is part of the PronounceableWord library.
+ *
+ * (c) Loic Chardonnet
+ *
+ * For the full copyright and license information, please view the LICENSE.txt
+ * file that was distributed with this source code.
+ */
+
+class PronounceableWord_DependencyInjectionContainer {
+}
---
+++ test/PronounceableWord/Tests/DependencyInjectionContainerTest.php
@@ -0,0 +1,14 @@
+<?php
+/*
+ * This file is part of the PronounceableWord library.
+ *
+ * (c) Loic Chardonnet
+ *
+ * For the full copyright and license information, please view the LICENSE.txt
+ * file that was distributed with this source code.
+ */
+
+require_once dirname(__FILE__) . '/../../../src/PronounceableWord/DependencyInjectionContainer.php';
+
+class PronounceableWord_Tests_DependencyInjectionContainerTest extends PHPUnit_Framework_TestCase {
+}
