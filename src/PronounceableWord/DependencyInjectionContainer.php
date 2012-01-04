9a5a91e44aeb3247bb524da966c2c518e61fd8cd
Loïc Chardonnet <loic.chardonnet@gmail.com>
2012-01-04 10:45:05 +0000

    [DependencyInjectionContainer] Adding LinkedLetters

--- src/PronounceableWord/DependencyInjectionContainer.php
+++ src/PronounceableWord/DependencyInjectionContainer.php
@@ -25,17 +25,24 @@ class PronounceableWord_DependencyInjectionContainer {
         $this->configurations['LinkedLetters'] = new PronounceableWord_Configuration_LinkedLetters();
         $this->configurations['LetterTypes'] = new PronounceableWord_Configuration_LetterTypes();
 
+        $this->classNames['LinkedLetters'] = 'PronounceableWord_LinkedLetters';
         $this->classNames['Generator'] = 'PronounceableWord_Generator';
     }
 
     public function getGenerator() {
         $generatorClass = $this->classNames['Generator'];
 
-        $linkedLetters = new PronounceableWord_LinkedLetters($this->configurations['LinkedLetters']);
         $letterTypes = new PronounceableWord_LetterTypes($this->configurations['LetterTypes']);
         $lastLettersConsecutiveTypes = new PronounceableWord_LastLettersConsecutiveTypes($letterTypes);
-        $generator = new $generatorClass($linkedLetters, $letterTypes, $lastLettersConsecutiveTypes, $this->configurations['Generator']);
+        $generator = new $generatorClass($this->getLinkedLetters(), $letterTypes, $lastLettersConsecutiveTypes, $this->configurations['Generator']);
 
         return $generator;
+    }
+
+    public function getLinkedLetters() {
+        $linkedLettersClass = $this->classNames['LinkedLetters'];
+        $linkedLettersInstance = new $linkedLettersClass($this->configurations['LinkedLetters']);
+
+        return $linkedLettersInstance;
     }
 }
